790756ee8501492e824092b1bcc156b518430db4
atsushi oshima <aboutagial@yahoo.co.jp>
2021-11-17 23:28:58 +0900

    init

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,23 +1,31 @@
-# require 'fileutils'
-# preload_app true
-# timeout 5
-# worker_processes 4
-# listen '/tmp/nginx.socket', backlog: 1024
+require 'fileutils'
+preload_app true
+timeout 5
+worker_processes 4
+listen '/tmp/nginx.socket', backlog: 1024
+
+before_fork do |server,worker|
+	FileUtils.touch('/tmp/app-initialized')
+end
+
+# # config/unicorn.rb に下記内容を記載
+# # ※パスは変更する。
+# # rails_root = File.expand_path('../../', __FILE__)
+# rails_root = File.expand_path('/Users/hoppy/workspace/kadai/nginx_app', __FILE__)
 #
-# before_fork do |server,worker|
-# 	FileUtils.touch('/tmp/app-initialized')
-# end
-
-# config/unicorn.rb に下記内容を記載
-# ※パスは変更する。
-rails_root = File.expand_path('../../', __FILE__)
-
-
-worker_processes 2
-working_directory rails_root
-
-listen "#{rails_root}/tmp/unicorn.sock"
-pid "#{rails_root}/tmp/unicorn.pid"
-
-stderr_path "#{rails_root}/log/unicorn_error.log"
-stdout_path "#{rails_root}/log/unicorn.log"
+#
+#
+# worker_processes 2
+# working_directory rails_root
+#
+# # listen "#{rails_root}/tmp/unicorn.sock"
+# # pid "#{rails_root}/tmp/unicorn.pid"
+# #
+# # stderr_path "#{rails_root}/log/unicorn_error.log"
+# # stdout_path "#{rails_root}/log/unicorn.log"
+#
+# listen "#{rails_root}/tmp/unicorn.sock"
+# pid "#{rails_root}/tmp/unicorn.pid"
+#
+# stderr_path "#{rails_root}/log/unicorn_error.log"
+# stdout_path "#{rails_root}/log/unicorn.log"
